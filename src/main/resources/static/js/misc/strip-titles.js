76f7e15721fd4bbe79d6d6da9a71bb39608c43f4
Selbi182 <shadow93x@gmail.com>
2026-04-14 10:03:35 +0200

    Extracted title strip logic to separate file and improved it

---
+++ src/main/resources/static/js/misc/strip-titles.js
@@ -0,0 +1,82 @@
+const USELESS_WORDS = [
+  "radio",
+  "anniversary",
+  "bonus",
+  "deluxe",
+  "special",
+  "edition",
+  "explicit",
+  "extended",
+  "expansion",
+  "expanded",
+  "version",
+  "ver\.",
+  "cover",
+  "original",
+  "single",
+  "ep",
+  "motion\\spicture",
+  "ost",
+  "sound.?track",
+  "theme",
+  "from",
+  "re.?master",
+  "re.?issue",
+  "re.?record",
+  "re.?imagine",
+  "mono",
+  "stereo",
+  "\\d{4}"
+];
+const WHITELISTED_WORDS = [
+  "instrumental",
+  "orchestral",
+  "symphonic",
+  "live",
+  "classic",
+  "demo",
+  "session",
+  "reprise",
+  "re.?mix",
+  "re.?visit",
+  "take",
+  "edit",
+  "alternate",
+  "alternative",
+  "alt"
+];
+
+function buildUselessWordRegex(words) {
+  const w = words.join("|");
+  return new RegExp(
+    "\\s*(?:"
+    + "\\([^)]*?(" + w + ")[^)]*?\\)\\s*$|"
+    + "\\[[^\\]]*?(" + w + ")[^\\]]*?\\]\\s*$|"
+    + "-\\s[^-]*?(" + w + ").*$|"
+    + ",\\s[^,]*?(" + w + ").*$"
+    + ")",
+    "ig"
+  );
+}
+const USELESS_WORDS_REGEX = buildUselessWordRegex(USELESS_WORDS);
+const USELESS_WORDS_REGEX_JUST_WHITELIST = buildUselessWordRegex(WHITELISTED_WORDS);
+const USELESS_WORDS_REGEX_WITH_WHITELIST = buildUselessWordRegex([...USELESS_WORDS, ...WHITELISTED_WORDS]);
+
+function separateUnimportantTitleInfo(title) {
+  const aggressive = isPrefEnabled("strip-titles-aggressive");
+  const uselessWordRegex = aggressive ? USELESS_WORDS_REGEX_WITH_WHITELIST : USELESS_WORDS_REGEX;
+  let index = title.search(uselessWordRegex);
+  let containsWhitelisted = title.search(USELESS_WORDS_REGEX_JUST_WHITELIST);
+  if (index >= 0 && (aggressive || containsWhitelisted < 0)) {
+    let mainTitle = title.substring(0, index);
+    let extraTitle = title.substring(index, title.length);
+    return {
+      main: mainTitle,
+      extra: extraTitle
+    };
+  }
+  return {
+    main: title,
+    extra: ""
+  };
+}
